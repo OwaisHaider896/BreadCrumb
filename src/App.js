a63408a0a6137cf81c9c6ccf0e370c0e81cff796
Owais Naqvi <65233517+OwaisHaider896@users.noreply.github.com>
2020-12-04 16:12:25 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./styles.css";
+import AngleDown from "./AngleDown";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="">
+        <nav className="flex justify-between m-4 p-2 shadow border border-white-100 rounded-full">
+          <div className=" flex justify-between  items-center">
+            <Home />
+            <h1 className="ml-2">Dashboard</h1>
+          </div>
+          <ul className="flex">
+            <img
+              className="inline object-cover w-8 h-8 mr-2 rounded-full"
+              src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+              alt="Profile"
+            />
+            <h2 className="flex font-bold text-sm items-center mr-4">
+              Justiminao Alves
+            </h2>
+            <li className="flex items-center mr-4">
+              <AngleDown />
+            </li>
+            <div className="text-sm text-left  dropdown font-bold text-white rounded absolute bg-purple-900  top-14 right-1 ">
+              <ul className="p-2">
+                <li className="p-2">Profile</li>
+                <li className="p-2">Signout</li>
+              </ul>
+            </div>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
